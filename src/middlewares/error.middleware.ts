c47209480e149be66399ed593a2ad101f75a965d
Abir-Al-Arafat <antorabir@gmail.com>
2026-08-12 09:59:18 +0600

    update: error tracing

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -9,9 +9,10 @@ export const errorMiddleware = (
   _next: NextFunction,
 ): void => {
   if (err instanceof AppError) {
+    console.error("err instanceof AppError", err);
     res.status(err.statusCode).json(ResponseBuilder.failure(err.message));
     return;
   }
-
+  console.error("err default case", err);
   res.status(500).json(ResponseBuilder.failure("Internal server error"));
 };
