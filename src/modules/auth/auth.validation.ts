f89af35ed875a471c73cc69feaeafc0cae44a0ba
Abir-Al-Arafat <antorabir@gmail.com>
2026-08-12 10:15:09 +0600

    update: bail

--- src/modules/auth/auth.validation.ts
+++ src/modules/auth/auth.validation.ts
@@ -5,37 +5,49 @@ export class AuthValidation {
     body("name")
       .exists({ values: "falsy" })
       .withMessage("Name is required")
+      .bail()
       .isString()
       .withMessage("Name must be a string")
+      .bail()
       .trim()
       .notEmpty()
-      .withMessage("Name cannot be empty"),
+      .withMessage("Name cannot be empty")
+      .bail(),
 
     body("email")
       .exists({ values: "falsy" })
       .withMessage("Email is required")
+      .bail()
       .isEmail()
       .withMessage("Invalid email format")
-      .normalizeEmail(),
+      .bail()
+      .normalizeEmail()
+      .bail(),
 
     body("password")
       .exists({ values: "falsy" })
       .withMessage("Password is required")
+      .bail()
       .isLength({ min: 6 })
-      .withMessage("Password must be at least 6 characters long"),
+      .withMessage("Password must be at least 6 characters long")
+      .bail(),
   ];
 
   static login = [
     body("email")
       .exists({ values: "falsy" })
       .withMessage("Email is required")
+      .bail()
       .isEmail()
       .withMessage("Invalid email format")
-      .normalizeEmail(),
+      .bail()
+      .normalizeEmail()
+      .bail(),
 
     body("password")
       .exists({ values: "falsy" })
       .withMessage("Password is required")
+      .bail()
       .notEmpty()
       .withMessage("Password cannot be empty"),
   ];
